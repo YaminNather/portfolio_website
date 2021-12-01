6af775994edb6a6d73d9e27fe091a0707b4ee171
Yamin Nather <2001s.yn@gmail.com>
2021-12-01 14:01:11 +0530

    Removed placeholder values for skills progressbar

--- src/home_page/skills_slide/skills_slide.tsx
+++ src/home_page/skills_slide/skills_slide.tsx
@@ -4,6 +4,23 @@ import SlideTitle from "../slide_title/slide_title";
 import './skills_slide_styles.scss';
 import SkillBar from "./skill_bar";
 
+interface Skill {
+  readonly name: string;
+  readonly experience: number;
+}
+
+const skills: Skill[] = [
+  { name: "HTML", experience: 85 },
+  { name: "CSS", experience: 80 },
+  { name: "Firebase", experience: 80 },
+  { name: "Javascript", experience: 80 },
+  { name: "Typescript", experience: 80 },
+  { name: "React", experience: 85 },  
+  { name: "Flutter", experience: 90 },
+  { name: "Express", experience: 80 },
+  { name: "Mongodb", experience: 80 },
+];
+
 const SkillsSlide: FC = (props) => {
   function render(): JSX.Element {
     return (
@@ -15,10 +32,10 @@ const SkillsSlide: FC = (props) => {
               <SlideTitle title="EXPERIENCE" overlay="MY SKILLS" />
 
               <div className='skills'>                
-                {new Array(8).fill(0).map(
+                {skills.map(
                   (value) => (
                     <div className="skill_container">
-                      <SkillBar className="skill" value={80} skillName="HTML" />
+                      <SkillBar className="skill" value={value.experience} skillName={value.name} />
                     </div>
                   )
                 )}
